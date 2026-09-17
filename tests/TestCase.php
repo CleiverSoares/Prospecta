c93c71f37f738e27f40164b28889ec2df04d0417
CleiverSoares <CleiverSoares@users.noreply.github.com>
2026-09-17 00:01:50 -0300

    ci: withoutVite nos testes e workflow mais robusto
    PHPUnit nao depende mais do manifest; CI cria sqlite, gera key e builda front com scripts.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Feature tests não precisam do manifest do Vite no CI
+        $this->withoutVite();
+    }
 }
